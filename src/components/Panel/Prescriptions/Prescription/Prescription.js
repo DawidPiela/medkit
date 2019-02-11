288ec3feee4c58d807120ba4da7e88f43c856d2a
dpiela <dawid.piela@coig.pl>
2019-02-11 16:50:49 +0100

    working with styles

--- src/components/Panel/Prescriptions/Prescription/Prescription.js
+++ src/components/Panel/Prescriptions/Prescription/Prescription.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 import styles from './Prescription.module.scss';
 
 class Prescription extends Component {
+
+  printDocument() {
+    const input = document.getElementById('divToPrint')
+    html2canvas(input)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL('image/png')
+        const pdf = new jsPDF()
+        pdf.addImage(imgData, 'JPEG', 0, 0)
+        pdf.save("download.pdf")
+      })
+  }
+
   render() {
     const prescriptions = []
     for (let dataName in this.props.prescriptionData) {
@@ -14,35 +28,61 @@ class Prescription extends Component {
       )
     }
 
+    let frequencyDiv = [];
+
     const prescriptionsOutput = prescriptions.map(data => {
-      let icon
       switch (data.name) {
-        case 'Afternoon': icon =
-          (<span><i
-            className='fas fa-cloud-moon'
-            aria-hidden='true'></i>
-          </span>)
-          break;
-        case 'Morning': icon =
-          (<span><i
-            className='fas fa-sun'
-            aria-hidden='true'></i>
-          </span>)
-          break
-        case 'Evening': icon =
-          (<span><i
-            className='fas fa-moon'
-            aria-hidden='true'></i>
-          </span>)
-          break
-        default: icon = null
+        case 'Medicine': return <div
+          key={data.name}
+          className={styles.medicine}>
+          <p>{data.data}</p>
+        </div>
+        case 'Dose': return <div
+          key={data.name}
+          className={styles.dose}>
+          {data.name}: {data.data}
+          <span>
+            <i className='fas fa-pills' aria-hidden='true'></i>
+          </span>
+        </div>
+        case 'Duration': return <div
+          key={data.name}
+          className={styles.duration}>
+          <p>{data.name}: {data.data}</p>
+        </div>
+        case 'Afternoon': frequencyDiv.push(<div
+          key={data.name}
+          className={styles.afternoon}>{data.name}: <span>
+            <i className='fas fa-cloud-moon' aria-hidden='true'></i>
+          </span>
+          {data.data}<br /></div>); return null;
+        case 'Morning': frequencyDiv.push(<div
+          key={data.name}
+          className={styles.morning}>{data.name}: <span>
+            <i className='fas fa-sun' aria-hidden='true'></i>
+          </span>
+          {data.data}<br /></div>); return null;
+        case 'Evening': frequencyDiv.push(<div
+          key={data.name}
+          className={styles.evening}>{data.name}: <span>
+            <i className='fas fa-moon' aria-hidden='true'></i>
+          </span>
+          {data.data}<br /></div>); return null;
+        default: return null
       }
-      return <span key={data.name}>{data.name}: {icon} {data.data}<br /></span>
     })
 
     return (
       <div className={styles.box}>
-        <p>{prescriptionsOutput}</p>
+        <div id="divToPrint" className={styles.mt4}>
+          {prescriptionsOutput}
+          <div className={styles.frequency}>
+            {frequencyDiv}
+          </div>
+        </div>
+        <div className="mb5">
+          <button className={styles.pdfButton} onClick={this.printDocument}>PDF</button>
+        </div>
       </div>
     )
   }
